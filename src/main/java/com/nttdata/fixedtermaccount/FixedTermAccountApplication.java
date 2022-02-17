c0b1f0ba5b6a5a412580d169c6d3c046ead10ee6
Emir Sanchez <emir2104@gmail.com>
2022-02-17 10:05:27 -0500

    first commit

---
+++ src/main/java/com/nttdata/fixedtermaccount/FixedTermAccountApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.fixedtermaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedTermAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixedTermAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/fixedtermaccount/FixedTermAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.fixedtermaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixedTermAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
